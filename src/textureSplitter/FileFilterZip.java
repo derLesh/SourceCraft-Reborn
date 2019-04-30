906121a85c7cdf2433cdb496b7caabfec4c7d673
Lesh <11671272+derLesh@users.noreply.github.com>
2019-04-30 10:49:10 +0200

    Imported TextureSplitter

---
+++ src/textureSplitter/FileFilterZip.java
@@ -0,0 +1,19 @@
+package textureSplitter;
+
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+public class FileFilterZip
+  extends FileFilter
+{
+  public FileFilterZip() {}
+  
+  public boolean accept(File f)
+  {
+    return (f.getName().toLowerCase().endsWith(".zip")) || (f.isDirectory());
+  }
+  
+  public String getDescription() {
+    return "ZIP Files";
+  }
+}
